87481fe37bfcc0e8eff7a383e7d97d414c98cdfe
merry <114328720+appin-cell@users.noreply.github.com>
2023-01-13 12:29:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pyautogui as gui
 import sys
-
+import pyautogui
+import time
+import win32gui
+app = win32gui.FindWindow(None,'Minecraft 1.19.3 - マルチプレイ（サードパーティーのサーバー）')
+time.sleep(1)
+win32gui.SetForegroundWindow(memoapp)
 
 try:
     while True:
