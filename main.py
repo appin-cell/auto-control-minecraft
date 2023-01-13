2edd564f43357d2788b7d30ef8915df871d7b172
merry <114328720+appin-cell@users.noreply.github.com>
2023-01-13 12:23:59 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import pyautogui as gui
+import sys
+
+
+try:
+    while True:
+       x,y = gui.position()
+       rgb = gui.pixel(x,y)
+       if rgb = 
+       
+
+       
+except KeyboardInterrupt:
+    print('\n終了')
+    sys.exit()
